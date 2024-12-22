1436432b3bd2e57a48b0a70bccf0374550ad6d40
groom115 <shyamabhishek115@gmail.com>
2024-12-21 23:28:48 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/header';
-import Profile from './Components/profile';
-import { useState, useEffect } from 'react';
-import { TabData } from './data/tab-data';
-import Tabs from './Components/tabs';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './views/login';
+import Details from './views/details';
 
 function App() {
 
-  const [activeTab, setActiveTab] = useState(0);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
-    <div className="App">
-      <div className='mx-10 my-10 mb-10'>
-        <Header windowWidth={windowWidth} />
-        <Tabs value={TabData} activeTab={activeTab} setActiveTab={setActiveTab} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/details" element={<Details />} />
 
-        {activeTab === 0 ? (
-          <div className="sm:grid sm:grid-cols-[2fr_3fr] gap-8 w-full mt-4 h-screen">
-            <div className="mb-3 sm:mb-6 " style={{ flex: 1 }}>
-              <img
-                src="images/map.png"
-                alt="oops"
-                className="w-full h-full object-cover rounded-lg sm:min-w-[600px]"
-                style={{ flex: 1 }}
-              />
-            </div>
-            <div className="mb-3 sm:mb-6">
-              <Profile windowWidth={windowWidth} />
-            </div>
-          </div>
-        ) : (
-          <div className="mt-10 text-center text-white">
-            <p>No content available for this tab.</p>
-          </div>
-        )}
-      </div>
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
